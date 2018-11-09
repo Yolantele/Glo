1a2808fe3f6028ea9dab9e5b8fc0c672c9b0d285
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-11-09 19:24:02 +0000

    removes GloNative front end

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,9 @@
 const theFramework = require("the-framework");
 
-theFramework.get("/hello", [{id: "name", type: theFramework.STRING, required: false, description: "Your name"}], {
-    description: "Says hello",
-    authRequired: false
-}, async (params, user) => {
-    // Params is an object of processed parameters
-    // user is the logged in user, if there is one
-    return {
-        // message: "Hello " + params.name
-        message: "Hello from Glo 🌺"
-    }
-});
+theFramework.get( "/hello", 
+[{ id: "name", type: theFramework.STRING, required: false, description: "Your name" }], 
+{ description: "Says hello", authRequired: false }, 
+async (params, user) => { 
+  return { message: "Hello from Glo! 🌺" } 
+})
+
